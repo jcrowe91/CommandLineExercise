cf4d4a3e95d47e945058596c15b387a17e7eb7f3
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 15:47:00 -0600

    Add project files.

---
+++ CommandLineExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandLineExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Changing per directions!");
+        }
+    }
+}
